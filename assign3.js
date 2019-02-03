8389cddf3934c44b7b9bf4ff0d7f5b7cabbf8e27
Meghan Mullally <ms.mullally27@gmail.com>
2019-02-03 14:02:44 -0800

    created loop, not sure how to get rid of the  thought

---
+++ assign3.js
@@ -0,0 +1,9 @@
+// assign 3 - lesson 3 
+
+// assign 3 - lesson 3 
+
+let arr = ['dog', 'cat', 'bird',  'fish', 'turtle'];
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(i, ',' , arr[i]);
+}
