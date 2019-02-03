e8cebf997c23b9619c7dc3c4e9f53ec81af3eeb3
Meghan Mullally <ms.mullally27@gmail.com>
2019-02-03 14:23:25 -0800

    deck of cards bonus assignment

---
+++ bonus5-03.js
@@ -0,0 +1,7 @@
+// bonus 
+let arr = [['spade', 'hearts', 'clubs', 'diamonds'], ['ace', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'jack', 'queen', 'king']];
+
+for (let x = 0; x < 4; x++) {
+ for (let z = 0; z < 13; z++)
+   console.log(arr[0][x], 'of' , arr[1][z])
+}
